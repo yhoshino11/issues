2b71ed2e21961b60b445ab244aa2a3bff25c54f1
Yu Hoshino <yhoshino11@gmail.com>
2015-10-14 00:00:45 +0900

    sort by ascending orders

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,19 @@
+defmodule Issues.GithubIssues do
+  @user_agent [ { "User-agent", "yhoshino11@gmail.com" } ]
+
+  # use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response(%{status_code: 200, body: body}), do: { :ok, :jsx.decode(body) }
+  def handle_response(%{status_code: __, body: body}), do: { :error, :jsx.decode(body) }
+end
